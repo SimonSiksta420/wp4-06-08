ecd2b91af6720cdad2ceaffe61d8546b1524a664
Šimon Siksta <simon.siksta@student.ossp.cz>
2022-03-02 11:32:14 +0100

    Čekám na tvrdý sex.

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -5,20 +5,26 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use App\Model\PostFacade;
+use App\Model\UserFacade;
 use Nette;
 use Nette\Application\UI\Form;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
 	private PostFacade $facade;
+	private UserFacade $userFacade;
 
-	public function __construct(PostFacade $facade)
+	public function __construct(PostFacade $facade, UserFacade $userFacade)
 	{
 		$this->facade = $facade;
+		$this->userFacade = $userFacade;
 	}
 
 	public function renderDefault(): void
 	{
+		$this->userFacade->add("Admin", "admin@ossp.cz", "secret");
+		$this->userFacade->add("DruhyUser", "second@ossp.cz", "heslo");
+
 		$this->template->posts = $this->facade
 			->getPublicArticles()
 			->limit(5);
